4181f12617e11e7bbc3002a04cea743dd75adeaa
HaniffAlisa <154397440+HaniffAlisa@users.noreply.github.com>
2024-01-29 18:04:22 +0000

    intro HW

---
+++ Menu.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Menu" << endl;
+    return 0;
+}
---
+++ class_schedule.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+string et575 = "ET-575 is a course in procedural programming and algorithmic concepts using the C++ language.";
+
+    cout<< "Course description:" << endl;
+
+    cout<< et575 << endl;
+        
+    cout<< "ET-575 Class Schedule:" << endl;
+
+    cout<< "Time \t Location\n";
+    cout<< "--------\t------------- \n";
+    cout<<"9:00 AM \t Room T18 \n";
+    
+
+
+    return 0;
+}
