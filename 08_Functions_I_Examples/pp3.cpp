2a9f2e4eb968189c8e67d8b696bb3460a07ead18
HaniffAlisa <154397440+HaniffAlisa@users.noreply.github.com>
2024-04-04 14:31:15 +0000

    update

---
+++ 08_Functions_I_Examples/pp3.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+void hello(string s);
+
+
+int main(){
+
+    string str ="Hello";  
+
+    cout<<string(s)<<endl;
+
+}
+void hello(string s){
+    cout<< "Hello"<<s<<endl;
+}
