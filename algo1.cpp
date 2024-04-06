ee09b81821d4b5b4b3b0058a81edbaf3f558588d
HaniffAlisa <154397440+HaniffAlisa@users.noreply.github.com>
2024-04-06 14:31:08 +0000

    update

---
+++ algo1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std; 
+
+int main (){
+
+    cout<< endl;
+
+    int n=0;
+
+    cout<< "Enter a value for n: "<<endl;
+    cin>>n;
+
+    for(int i=n; i<=n; ++i){
+        cout<< "* ";
+    }
+cout<<"\n";
+
+return 0;
+}
